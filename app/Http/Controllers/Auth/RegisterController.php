93b9cb008c7a60bf009704e7b75b9484aac1c24b
mass.web.app <mass.web.app@gmail.com>
2023-08-19 12:58:02 -0700

    implement verified user after register && added regex for password and mobile

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use App\Rules\ValidMobileRule;
+use App\Rules\ValidPasswordRule;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -52,7 +54,8 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'mobile' => ['required', 'string', 'min:9', 'max:14', 'unique:users',new ValidMobileRule()],
+            'password' => ['required', 'string', 'min:8', 'confirmed',new ValidPasswordRule()],
         ]);
     }
 
@@ -64,6 +67,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        dd('passwesz');
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,10 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('mobile')->nullable()->unique();
+            $table->string('bio')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('status',['active','inactive','ban'])->default('active');
             $table->rememberToken();
             $table->timestamps();
         });
