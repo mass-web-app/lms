bc88b3cd75e8569d55ffeb7b9ba0d7e824d999ba
mass.web.app <mass.web.app@gmail.com>
2023-09-09 12:24:00 -0700

    define the first module for project

---
+++ modules/Mass/User/Providers/UserServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mass\User\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class UserServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+
+    }
+
+}
